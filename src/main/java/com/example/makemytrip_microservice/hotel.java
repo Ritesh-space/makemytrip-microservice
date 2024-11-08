1e15dafea05a024fbe93866470cf143b80ae2a04
tronX1000 <147911813+tronX1000@users.noreply.github.com>
2024-11-08 16:32:45 +0530

    coderefactor

---
+++ src/main/java/com/example/makemytrip_microservice/hotel.java
@@ -0,0 +1,10 @@
+package com.example.makemytrip_microservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hotel {
+    @GetMapping("/hotel")
+    public String getData() {return "Please book your hotel ticket in mumbai 99% discount" ; }
+}
